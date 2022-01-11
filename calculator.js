9f8c61920594654dc2d671c10b6315d74aff5a12
Mike Nieva <m@mikenieva.com>
2022-01-11 11:14:57 -0600

    Suma realizada

--- calculator.js
+++ calculator.js
@@ -1,6 +1,27 @@
+console.log("hola mundo")
+
+// FUNCIONES DECLARATIVAS
+// SOLO SE HACE UNA VEZ
+// function
 function sum(a, b) {
-  return;
+
+  // EN CASO DE QUE NO HAYA ARGUMENTOS
+  if (!a && !b) {
+    return 0
+  }
+
+  // EN CASO DE QUE EL SEGUNDO ARGUMENTO NO EXISTA
+  if(!b) {
+    b = 0
+  }
+
+  const total = a + b  
+
+  return total;
 }
+
+// INVOCACIÓN DE FUNCIÓN
+// PUEDE USARASE (INVOCARSE) LAS VECES QUE QUIERAN
 
 function subtract(a, b) {
   return;
